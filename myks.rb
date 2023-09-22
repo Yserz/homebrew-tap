c55604d52fb1c0e0a120906125fb916d5475d22d
Michael Koppen <Yserz@users.noreply.github.com>
2023-09-22 16:53:12 +0200

    fix: remove version prefix

--- myks.rb
+++ myks.rb
@@ -2,9 +2,9 @@ class Myks < Formula
   desc "Maintain configuration of many applications for multiple Kubernetes clusters"
   homepage "https://github.com/mykso/myks"
   url "https://github.com/mykso/myks/releases/download/v2.0.0/myks_2.0.0_darwin_arm64.tar.gz"
-  version "v2.0.0"
-  version_scheme 1
+  version "2.0.0"
   sha256 "f612d1341f2c7bf89727fa9f5fcf21ada926063988261c90c38f86fb2ce37daf"
+  version_scheme 1
   license "MIT"
 
   def install
