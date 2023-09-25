43d139a5be4d2ad45a58769206c319aa3d8fc754
Michael Koppen <Yserz@users.noreply.github.com>
2023-09-25 13:42:54 +0200

    feat: build myks from source

--- myks.rb
+++ myks.rb
@@ -1,11 +1,35 @@
 class Myks < Formula
   desc "Maintain configuration of many applications for multiple Kubernetes clusters"
   homepage "https://github.com/mykso/myks"
-  url "https://github.com/mykso/myks/releases/download/v2.0.1/myks_2.0.1_darwin_amd64.tar.gz"
-  version "2.0.1"
-  sha256 "1522c570becb471e05854aee6cdf04a1a19e34026331eb77aec912c4c680152d"
+  url "https://github.com/mykso/myks.git",
+      tag:      "v2.0.0"
+  version "2.0.0"
   license "MIT"
   version_scheme 1
+
+  # if OS.mac?
+  #   if Hardware::CPU.arm?
+  #     url "https://github.com/carvel-dev/ytt/releases/download/v0.46.0/ytt-darwin-arm64"
+  #     sha256 "0c025f1210875f185a867e6cb3fd4770627f13ce64d69b6092de713464395ddb"
+  #   else
+  #     url "https://github.com/carvel-dev/ytt/releases/download/v0.46.0/ytt-darwin-amd64"
+  #     sha256 "a2ca42f8178ba2849076b580177c5c12769e17f01a61ab50b106367728004692"
+  #   end
+  # elsif OS.linux?
+  #   if Hardware::CPU.arm?
+  #     url "https://github.com/carvel-dev/ytt/releases/download/v0.46.0/ytt-linux-arm64"
+  #     sha256 "9552c863452622386b9e9fcab0eb0533b6e8c77bc3f5c753967d40232b2a1721"
+  #   else
+  #     url "https://github.com/carvel-dev/ytt/releases/download/v0.46.0/ytt-linux-amd64"
+  #     sha256 "348cb34965b64c07fd5118e69efd9a4fae7e22f57db4e91e2d9903c1ad19f041"
+  #   end
+  # end
+
+  depends_on "go" => :build
+  depends_on "helm"
+  depends_on "ytt"
+  depends_on "vendir"
+
 
   def install
     bin.install "myks"
