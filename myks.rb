83e783dbacf82b71b9f968df74538c7f765f5aa2
Michael Koppen <Yserz@users.noreply.github.com>
2023-09-27 10:09:17 +0200

    refactor: cleanup CI (#2)

--- myks.rb
+++ myks.rb
@@ -7,24 +7,6 @@ class Myks < Formula
   license "MIT"
   version_scheme 1
 
-  # if OS.mac?
-  #   if Hardware::CPU.arm?
-  #     url "https://github.com/carvel-dev/ytt/releases/download/v0.46.0/ytt-darwin-arm64"
-  #     sha256 "0c025f1210875f185a867e6cb3fd4770627f13ce64d69b6092de713464395ddb"
-  #   else
-  #     url "https://github.com/carvel-dev/ytt/releases/download/v0.46.0/ytt-darwin-amd64"
-  #     sha256 "a2ca42f8178ba2849076b580177c5c12769e17f01a61ab50b106367728004692"
-  #   end
-  # elsif OS.linux?
-  #   if Hardware::CPU.arm?
-  #     url "https://github.com/carvel-dev/ytt/releases/download/v0.46.0/ytt-linux-arm64"
-  #     sha256 "9552c863452622386b9e9fcab0eb0533b6e8c77bc3f5c753967d40232b2a1721"
-  #   else
-  #     url "https://github.com/carvel-dev/ytt/releases/download/v0.46.0/ytt-linux-amd64"
-  #     sha256 "348cb34965b64c07fd5118e69efd9a4fae7e22f57db4e91e2d9903c1ad19f041"
-  #   end
-  # end
-
   depends_on "go" => :build
   depends_on "git"
   depends_on "helm"
